3f42b04bd599bea2bda4f20e140ee6ee182eec7a
George Olive <george@universalcogs.com>
2013-06-12 16:10:02 -0400

    Initial commit
    	new file:   speak7330.py
    			This file generates the 7330 codes to speak the phrase supplied
    
    	new file:   spoken_words.csv
    			Dictionary of phrases 7330 speaks in csv format

---
+++ speak7330.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+
+import sys
+import re
+import argparse
+
+class Phrase(object):
+    def __init__(self, s=""):
+        super(Phrase, self).__init__()
+        self.p = s.lower().split()
+    def append(self, a):
+        self.p.append(a)
+    def pop(self):
+        return self.p.pop()
+    def num_words(self):
+        return len(self.p)
+    def __len__(self):
+        return len(self.p)
+    def __str__(self):
+        return ' '.join(self.p)
+    def __hash__(self):
+        return hash(str(self))
+    def __repr__(self):
+        return str(self)
+    def __cmp__(self,other):
+        return cmp(str(self), str(other))
+                 
+expando_words = re.compile('^[ankw].*[0-9][a-z]{1,3}$|^[0-9]{2,}$')
+ 
+class Words(object):
+    def __init__(self, str):
+        super(Words, self).__init__()
+        self.string = str.lower().split()
+        self.__words = []
+    def __iter__(self):
+        self.__words = [x for x in self.string]
+        return self
+    def next(self):
+        if self.__words:
+            x = self.pop(0)
+            if expando_words.match(x):
+                self.push_letters(x)
+                x = self.pop(0)
+            return x
+        else:
+            raise StopIteration
+    def push_letters(self, word):
+        for i in word[::-1]:
+            self.push(i)
+    def push(self,v):
+        self.__words.insert(0, v)
+    def pop(self,v):
+        return self.__words.pop(0)
+    def __len__(self):
+        return len(self.__words)
+    def substrings(self):
+        for i in range(len(self.string)-1):
+            yield ' '.join(self.string[:i+1])
+
+class Emitter(object):
+    def __init__(self):
+        super(Emitter, self).__init__()
+        self.output = []
+    def emit(self, s):
+        self.output.append(s)
+    def send(self):
+        print " ".join(self.output)
+
+def speak(q):
+    phrase = Phrase()
+    output = Emitter()
+    words = Words(q)
+    
+    for word in words:
+        phrase.append(word)
+
+        if phrase in nonterminals and len(words) > 0:
+            continue
+        else:
+            try:
+                tok = terminals[phrase]
+                output.emit(tok)
+                if args.verbose:
+                    print '    Found:', tok, phrase
+            except:
+                # walk back popping one token at a time from built up phrase
+                if len(phrase) > 1:
+                    while len(phrase) > 1:
+                        words.push(phrase.pop())
+                        try:
+                            tok = terminals[phrase]
+                            output.emit(tok)
+                            if args.verbose:   
+                                print "    Found:", tok, phrase
+                            break
+                        except KeyError:
+                            pass
+                    else:
+                        # we should never get here because at the very least the first token was valid
+                        raise Exception("Parse walkback failed")
+                else:
+                    if args.verbose:
+                        print "Not Found:     ", phrase
+        phrase = Phrase()
+    return output
+    
+parser = argparse.ArgumentParser(description="Generate speech codes for 7330 repeater")
+parser.add_argument('-v', dest='verbose', action='store_true',
+                    help='Print parsing detail')
+parser.add_argument('-e', dest='expression', nargs='?', metavar="PHRASE",
+                    help='speak PHRASE')
+args = parser.parse_args()
+
+
+
+terminals = {}
+nonterminals = set()
+
+with open('spoken_words.csv', 'r') as f:
+    for l in f:
+        code, string = l.strip().lower().split(',')
+        terminals[Phrase(string)] = code
+
+        for p in Words(string).substrings():
+            nonterminals.add(Phrase(p))
+
+if False:
+    pprint(sorted(terminals))
+    pprint(nonterminals)
+    exit(0)
+
+
+if args.expression:
+    speak(args.expression).send()
+else:
+    while True:
+        try:
+            q = raw_input("Enter phrase: ").strip().lower()
+            if q == "":
+                exit(0)
+        except:
+            print
+            break
+        output = speak(q)
+        if args.verbose:
+            print "Result String:"
+        output.send()
